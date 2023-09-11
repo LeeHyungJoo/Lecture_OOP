57c3ebd6e8a97f2826db414cfc25c52aa532ef23
Yodle_94 <sj_artist@naver.com>
2023-09-11 15:38:24 +0900

    [DI] 적용 전

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var btn = new Button(new Lamp());
+btn.Click();
+btn.Click();
+btn.Click();
+
+
+class Lamp
+{
+    public Lamp() { }
+    public void TurnOn() { Console.WriteLine("turn on"); }
+    public void TurnOff() { Console.WriteLine("turn off"); }
+}
+
+class Button
+{
+    public Button(Lamp lamp) { this.lamp = lamp; }
+
+    public void Click()
+    {
+        if (bPressed)
+            lamp?.TurnOff();
+        else
+            lamp?.TurnOn();
+
+        bPressed = !bPressed;
+    }
+
+    private bool bPressed;
+    private Lamp lamp;
+}
