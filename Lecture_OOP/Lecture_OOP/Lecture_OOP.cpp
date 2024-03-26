5e9a495495caf535c72d7588aa8da7253ebd371e
HyungJoo <prism9411@gmail.com>
2024-03-26 18:44:36 +0900

    옵저버 패턴 구현 중.

--- Lecture_OOP/Lecture_OOP/Lecture_OOP.cpp
+++ Lecture_OOP/Lecture_OOP/Lecture_OOP.cpp
@@ -1,6 +1,57 @@
 ﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class IObserver
+{
+public:
+	virtual void Notify() = 0;
+};
+
+class StatusObserver : public IObserver
+{
+public:
+	virtual void Notify() override
+	{
+		cout << "status Noti" << endl;
+	};
+};
+
+class DistanceObserver : public IObserver
+{
+public:
+	virtual void Notify() override
+	{
+		cout << "distance Noti" << endl;
+	};
+};
+
+class Subject
+{
+public:
+	void RegistObserver(IObserver * observer)
+	{
+		m_observers.push_back(observer);
+	}
+
+	void NotifyAll()
+	{
+		for (const auto& ob : m_observers)
+			ob->Notify();
+	}
+
+private:
+	vector<IObserver*> m_observers;
+};
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Subject* sub = new Subject();
+	sub->RegistObserver(new StatusObserver());
+	sub->RegistObserver(new DistanceObserver());
+
+	sub->NotifyAll();
 }
